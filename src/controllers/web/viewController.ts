6767edff7561eb3de125458d7a65529957d35c32
myatmonphyusin2022-spec <myatmonphyusin2022@gmail.com>
2026-09-02 13:16:59 +0630

    Updated about.ejs,view.ts,viewController.ts

--- src/controllers/web/viewController.ts
+++ src/controllers/web/viewController.ts
@@ -1,5 +1,12 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 
-export const home=(req: Request, res: Response, next: NextFunction) => {
-    res.render("index", { title: "Home Screen" });
-}
+export const home = (req: Request, res: Response, next: NextFunction) => {
+  res.render("index", { title: "Home Screen" });
+};
+export const about = (req: Request, res: Response, next: NextFunction) => {
+  const users = [
+    { name: "John Doe", age: 30 },
+    { name: "Jane Smith", age: 25 },
+  ];
+  res.render("about", { title: "About Us", users });
+};
