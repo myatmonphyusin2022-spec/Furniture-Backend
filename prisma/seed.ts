e42efb5ee501e50e7bd8a79c330d4a109a415fcb
myatmonphyusin2022-spec <myatmonphyusin2022@gmail.com>
2026-09-10 20:45:19 +0630

    Updated authServices,seed.ts.schema.prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,6 @@ import { PrismaClient, Prisma } from "../src/generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 import * as bcrypt from "bcrypt";
-import { faker } from "@faker-js/faker";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
@@ -12,45 +11,34 @@ const prisma = new PrismaClient({
   adapter,
 });
 
-// const userData: Prisma.UserCreateInput[] = [
-//   {
-//     phone: "778661260",
-//     password: "",
-//     randToken: "sfwfx23rbkxg982ntxf87",
-//   },
-//   {
-//     phone: "778661261",
-//     password: "",
-//     randToken: "sfwfx23rbkxg982ntxf87",
-//   },
-//   {
-//     phone: "778661262",
-//     password: "",
-//     randToken: "sfwfx23rbkxg982ntxf87",
-//   },
-//   {
-//     phone: "778661263",
-//     password: "",
-//     randToken: "sfwfx23rbkxg982ntxf87",
-//   },
-//   {
-//     phone: "778661264",
-//     password: "",
-//     randToken: "sfwfx23rbkxg982ntxf87",
-//   },
-// ];
-
-function createRandomUser() {
-  return {
-    phone: faker.phone.number(),
+// သင့် ကုတ်မူရင်းအတိုင်း array ထဲတွင် password ကို "" အဖြစ်ထားရှိခြင်း
+const userData: Prisma.UserCreateInput[] = [
+  {
+    phone: "+959778661260",
     password: "",
-    randToken: faker.internet.jwt(),
-  };
-}
-
-export const userData = faker.helpers.multiple(createRandomUser, {
-  count: 5,
-});
+    randToken: "sfwfx23rbkxg982ntxf87",
+  },
+  {
+    phone: "+959778661261",
+    password: "",
+    randToken: "sfwfx23rbkxg982ntxf87",
+  },
+  {
+    phone: "+959778661262",
+    password: "",
+    randToken: "sfwfx23rbkxg982ntxf87",
+  },
+  {
+    phone: "+959778661263",
+    password: "",
+    randToken: "sfwfx23rbkxg982ntxf87",
+  },
+  {
+    phone: "+959778661264",
+    password: "",
+    randToken: "sfwfx23rbkxg982ntxf87",
+  },
+];
 
 async function main() {
   console.log(`Start seeding ...`);
@@ -58,7 +46,7 @@ async function main() {
   const password = await bcrypt.hash("12345678", salt);
 
   for (const u of userData) {
-    u.password = password;
+    u.password = password; // bcrypt hash လုပ်ထားသော password ဖြင့် အစားထိုးခြင်း
     await prisma.user.create({
       data: {
         phone: u.phone,
